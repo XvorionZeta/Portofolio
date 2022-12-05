afa073037988302a53f9a92aedbe69eeee19a8a6
XvorionZeta <zulfikar.h9050@gmail.com>
2022-12-06 00:18:38 +0700

    foto

--- src/pages/AboutMe.vue
+++ src/pages/AboutMe.vue
@@ -2,45 +2,50 @@
     <div class="flex justify-center h-screen">
         <div class="flex justify-start flex-col mx-auto mt-[20vh] max-w-4xl gap-7 pr-8">
             <h3 class="text-3xl text-slate-100 font-bold">About Me</h3>
-            <div class="max-w-[56ch]">
-                <p class="text-slate-400 text-lg"> Hello, My name is Zulfikar Hidayatullah, i’m a full
-                    stack
-                    web
-                    developer, from high school i enjoy creating innovation and doing some data analytic in my
-                    envirovment, and since 2015 <br> i really
-                    admire how fast technology have developed in present so i decided to learn how to create thing and
-                    that lead me to learn web developing.</p>
-                <p class="text-slate-400 max-w-sm text-lg mt-6">
-                    And so in the past 7 Years i have build several project for <span
-                        class="text-cyan-400 font-medium">start
-                        ups</span> and some big company like <span class="text-cyan-400 font-medium">PERUM
-                        PERURI</span>.
-                </p>
+            <div class="grid grid-cols-2 gap-6">
+                <div class="max-w-[56ch]">
+                    <p class="text-slate-400 text-lg"> Hello, My name is Zulfikar Hidayatullah, i’m a full
+                        stack
+                        web
+                        developer, from high school i enjoy creating innovation and doing some data analytic in my
+                        envirovment, and since 2015 <br> i really
+                        admire how fast technology have developed in present so i decided to learn how to create thing
+                        and
+                        that lead me to learn web developing.</p>
+                    <p class="text-slate-400 max-w-sm text-lg mt-6">
+                        And so in the past 7 Years i have build several project for <span
+                            class="text-cyan-400 font-medium">start
+                            ups</span> and some big company like <span class="text-cyan-400 font-medium">PERUM
+                            PERURI</span>.
+                    </p>
 
-                <p class="text-slate-400 max-w-sm text-lg mt-6">
-                    Here are some technology that i working with in my project :
-                <ul>
-                    <li class="text-slate-400 max-w-sm  pl-4 pt-4 flex gap-4">
-                        <div class="rounded-full bg-cyan-400 p-1 w-1 h-1 my-auto"></div> PHP (Vanilla,Laravel,CI)
-                    </li>
-                    <li class="text-slate-400 max-w-sm pl-4 pt-2 flex gap-4">
-                        <div class="rounded-full bg-cyan-400 p-1 w-1 h-1 my-auto"></div> CSS
-                        (Vanilla,Tailwind,Bootstrap)
-                    </li>
-                    <li class="text-slate-400 max-w-sm  pl-4 pt-2 flex gap-4">
-                        <div class="rounded-full bg-cyan-400 p-1 w-1 h-1 my-auto"></div> JS
-                        (Vanilla,Vue,Alpine)
-                    </li>
-                    <li class="text-slate-400 max-w-sm  pl-4 pt-2 flex gap-4">
-                        <div class="rounded-full bg-cyan-400 p-1 w-1 h-1 my-auto"></div> Swift
-                    </li>
-                    <li class="text-slate-400 max-w-sm  pl-4 pt-2 flex gap-4">
-                        <div class="rounded-full bg-cyan-400 p-1 w-1 h-1 my-auto"></div> Flutter
-                    </li>
-                </ul>
-                </p>
+                    <p class="text-slate-400 max-w-sm text-lg mt-6">
+                        Here are some technology that i working with in my project :
+                    <ul>
+                        <li class="text-slate-400 max-w-sm  pl-4 pt-4 flex gap-4">
+                            <div class="rounded-full bg-cyan-400 p-1 w-1 h-1 my-auto"></div> PHP (Vanilla,Laravel,CI)
+                        </li>
+                        <li class="text-slate-400 max-w-sm pl-4 pt-2 flex gap-4">
+                            <div class="rounded-full bg-cyan-400 p-1 w-1 h-1 my-auto"></div> CSS
+                            (Vanilla,Tailwind,Bootstrap)
+                        </li>
+                        <li class="text-slate-400 max-w-sm  pl-4 pt-2 flex gap-4">
+                            <div class="rounded-full bg-cyan-400 p-1 w-1 h-1 my-auto"></div> JS
+                            (Vanilla,Vue,Alpine)
+                        </li>
+                        <li class="text-slate-400 max-w-sm  pl-4 pt-2 flex gap-4">
+                            <div class="rounded-full bg-cyan-400 p-1 w-1 h-1 my-auto"></div> Swift
+                        </li>
+                        <li class="text-slate-400 max-w-sm  pl-4 pt-2 flex gap-4">
+                            <div class="rounded-full bg-cyan-400 p-1 w-1 h-1 my-auto"></div> Flutter
+                        </li>
+                    </ul>
+                    </p>
+                </div>
+                <div>
+                    <img src="../../public/FotoProfile.jpeg" class="rounded-2xl max-w-xs mx-auto">
+                </div>
             </div>
-
         </div>
     </div>
 </template>
